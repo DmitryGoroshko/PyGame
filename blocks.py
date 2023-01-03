bf14d6196f915a84222b02390c2ca467077d54df
Dmitry Goroshko <goroshkodmitry@yandex.ru>
2023-01-03 13:51:11 +0400

    [Task-08] Blocks texture added

--- blocks.py
+++ blocks.py
@@ -8,5 +8,6 @@ class Blocks(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-        self.image.fill(Color(PLATFORM_COLOR))
+        # self.image.fill(Color(PLATFORM_COLOR))
+        self.image = image.load("brick05.png")
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
